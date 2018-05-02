540064d4a3ca34f257b7dd28996e58afb68ad801
Taylor Lassiter <lassit09@gmail.com>
2018-05-01 22:51:02 -0400

    set up foundation of app and basic UI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'; //import React library
 import ReactDOM from 'react-dom'; //import ReactDOM library
 import Main from './Components/Main'
+import "./styles/stylesheet.css"
 
 ReactDOM.render(<Main/>, document.getElementById('root'));
